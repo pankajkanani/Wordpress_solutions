39ddcd4907b3ae699a74f91696f320244dd859bd
pankajcmarix <pankaj.cmarix@gmail.com>
2019-01-30 17:44:22 +0530

    change file name

--- functions/custompostcreate.php
+++ functions/custompostcreate.php
@@ -1,43 +1,43 @@
 <?php
-// Register Custom Post Type award
-// Post Type Key: award
-function create_award_cpt() {
+// Register Custom Post Type Team
+// Post Type Key: team
+function create_team_cpt() {
 
 	$labels = array(
-		'name' => __( 'Awards', 'Post Type General Name', 'backerei' ),
-		'singular_name' => __( 'Award', 'Post Type Singular Name', 'backerei' ),
-		'menu_name' => __( 'Award', 'backerei' ),
-		'name_admin_bar' => __( 'award', 'backerei' ),
-		'archives' => __( 'Award Archives', 'backerei' ),
-		'attributes' => __( 'Award Attributes', 'backerei' ),
-		'parent_item_colon' => __( 'Parent Award:', 'backerei' ),
-		'all_items' => __( 'All Awards', 'backerei' ),
-		'add_new_item' => __( 'Add New Award', 'backerei' ),
+		'name' => __( 'Team', 'Post Type General Name', 'backerei' ),
+		'singular_name' => __( 'Team', 'Post Type Singular Name', 'backerei' ),
+		'menu_name' => __( 'Team', 'backerei' ),
+		'name_admin_bar' => __( 'Team', 'backerei' ),
+		'archives' => __( 'Team Archives', 'backerei' ),
+		'attributes' => __( 'Team Attributes', 'backerei' ),
+		'parent_item_colon' => __( 'Parent Team:', 'backerei' ),
+		'all_items' => __( 'All Team', 'backerei' ),
+		'add_new_item' => __( 'Add New Team', 'backerei' ),
 		'add_new' => __( 'Add New', 'backerei' ),
-		'new_item' => __( 'New Award', 'backerei' ),
-		'edit_item' => __( 'Edit Award', 'backerei' ),
-		'update_item' => __( 'Update Award', 'backerei' ),
-		'view_item' => __( 'View Award', 'backerei' ),
-		'view_items' => __( 'View Awards', 'backerei' ),
-		'search_items' => __( 'Search Award', 'backerei' ),
+		'new_item' => __( 'New Team', 'backerei' ),
+		'edit_item' => __( 'Edit Team', 'backerei' ),
+		'update_item' => __( 'Update Team', 'backerei' ),
+		'view_item' => __( 'View Team', 'backerei' ),
+		'view_items' => __( 'View Team', 'backerei' ),
+		'search_items' => __( 'Search Team', 'backerei' ),
 		'not_found' => __( 'Not found', 'backerei' ),
 		'not_found_in_trash' => __( 'Not found in Trash', 'backerei' ),
 		'featured_image' => __( 'Featured Image', 'backerei' ),
 		'set_featured_image' => __( 'Set featured image', 'backerei' ),
 		'remove_featured_image' => __( 'Remove featured image', 'backerei' ),
 		'use_featured_image' => __( 'Use as featured image', 'backerei' ),
-		'insert_into_item' => __( 'Insert into Award', 'backerei' ),
-		'uploaded_to_this_item' => __( 'Uploaded to this Award', 'backerei' ),
-		'items_list' => __( 'Awards list', 'backerei' ),
-		'items_list_navigation' => __( 'Awards list navigation', 'backerei' ),
-		'filter_items_list' => __( 'Filter Awards list', 'backerei' ),
+		'insert_into_item' => __( 'Insert into Team', 'backerei' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Team', 'backerei' ),
+		'items_list' => __( 'Team list', 'backerei' ),
+		'items_list_navigation' => __( 'Team list navigation', 'backerei' ),
+		'filter_items_list' => __( 'Filter Team list', 'backerei' ),
 	);
 	$args = array(
-		'label' => __( 'Award', 'backerei' ),
+		'label' => __( 'Team', 'backerei' ),
 		'description' => __( '', 'backerei' ),
 		'labels' => $labels,
-		'menu_icon' => 'dashicons-awards',
-		'supports' => array('title', 'editor', 'thumbnail', 'revisions', 'author', 'page-attributes', 'post-formats', ),
+		'menu_icon' => 'dashicons-networking',
+		'supports' => array('title', 'editor', 'thumbnail', 'revisions', 'author', 'page-attributes', 'post-formats', 'custom-fields', ),
 		'taxonomies' => array('Category', ),
 		'public' => true,
 		'show_ui' => true,
@@ -53,7 +53,74 @@ function create_award_cpt() {
 		'publicly_queryable' => true,
 		'capability_type' => 'post',
 	);
-	register_post_type( 'award', $args );
+	register_post_type( 'team', $args );
 
 }
-add_action( 'init', 'create_award_cpt', 0 );
+add_action( 'init', 'create_team_cpt', 0 );
+
+
+// Register Taxonomy Category
+// Taxonomy Key: category
+function create_category_tax() {
+
+	$labels = array(
+		'name'              => _x( 'Categories', 'taxonomy general name', 'backerei' ),
+		'singular_name'     => _x( 'Category', 'taxonomy singular name', 'backerei' ),
+		'search_items'      => __( 'Search Categories', 'backerei' ),
+		'all_items'         => __( 'All Categories', 'backerei' ),
+		'parent_item'       => __( 'Parent Category', 'backerei' ),
+		'parent_item_colon' => __( 'Parent Category:', 'backerei' ),
+		'edit_item'         => __( 'Edit Category', 'backerei' ),
+		'update_item'       => __( 'Update Category', 'backerei' ),
+		'add_new_item'      => __( 'Add New Category', 'backerei' ),
+		'new_item_name'     => __( 'New Category Name', 'backerei' ),
+		'menu_name'         => __( 'Category', 'backerei' ),
+	);
+	$args = array(
+		'labels' => $labels,
+		'description' => __( '', 'backerei' ),
+		'hierarchical' => false,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'show_in_nav_menus' => true,
+		'show_in_rest' => false,
+		'show_tagcloud' => true,
+		'show_in_quick_edit' => true,
+		'show_admin_column' => false,
+	);
+	register_taxonomy( 'team_category', array('team', ), $args );
+
+}
+add_action( 'init', 'create_category_tax' );
+
+
+function my_recent_post()
+ {
+  global $post;
+
+  
+
+  $my_query = new WP_Query( array(
+       'post_type' => 'team',
+       'posts_per_page' => 3
+  ));
+  echo '<div class="error_team_section">';
+  if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+  	?> 
+  		<div class="col-md-4 col-sm-12 text-center">
+	  	  	<h4 class="designation"><?php echo get_field( "designation" ); ?></h4>
+			<h3 class="blog-title team-member-name"><?php the_title(); ?></h3>
+			<div> <?php the_post_thumbnail('medium'); ?></div>
+		</div>
+<?php
+  endwhile; endif;
+  
+  echo '</div>';
+
+  echo '<hr class="error-separator">';
+ }
+ add_shortcode( 'teamrecent', 'my_recent_post' );
+
+?>
--- functions/customposttemplate.php
+++ functions/customposttemplate.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: award Page
+ * Template Name: Team Page
  *
  * this page show all post here.
  *
@@ -11,76 +11,40 @@
 
 get_header(); ?>
 
-<?php $default_sidebar_position = get_theme_mod( 'default_sidebar_position', 'right' ); ?>
-<div class="blog-image-banner">
-	<?php the_post_thumbnail(); ?>
-</div>
+
 <div class="container">
-	<div class="row award">
+	<div class="row">
 		<div class="col-md-12 ">
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main">
-					<div class="blog-page-titles">
-						<h2 class="blog-main-title"><?php echo get_field( "page_title" );?></h2>
-						<p class="blog-sub-title"><?php echo get_field( "page_description" );?></p>
-					</div>
-					<?php 
-						$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-						$args = array('post_type' => 'award', 'posts_per_page' => 6, 'paged' => $paged, 'order' => 'ASC' );
+					
+					<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+						$args = array(
+							'post_type' => 'team', 
+							'posts_per_page' => 7, 
+							'paged' => $paged, 
+							'order' => 'ASC' 
+						);
 						query_posts($args); 
 					?>		
-					<div class="full-size-blog">
+					<div class="team">
+						
 						<?php // Start the loop 
 							if ( have_posts() ) : 
 								while (have_posts()) : the_post(); 
-									 ?>
-									    <div class="half-blog-inner">
-									        <div class="blog-inner-details">
-									           <div class="blog-image award-image">
-									           		<?php 
-									           	if (get_field('embedded_video') != '') {
-									           		echo get_field('embedded_video');
-									           	} else {?>
-									           		<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('full'); ?></a>			
-									           	<?php }  	 ?>
-									           	</div>
-									           	<h3 class="blog-title"><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h3>
-									           	<p class="awrd-subtitle"><?php if (get_field( "award_sub_title" )) {
-									           		echo get_field( "award_sub_title" );
-									           	}  ?></p>
-										    </div>      
-											<div class="blog-details"><?php 
-										        echo '<div class="blog-description">'.the_excerpt().'</div>'; ?>
-										    </div>
-										    <div class="blog-readmore"><a href="<?php the_permalink(); ?>" >weiter lesen</a></div>
-										</div>
-							<?php endwhile; ?>
+									the_post_thumbnail('medium');
+									the_title();
+									the_content();
+								endwhile; 
+							endif;
+						?>
 					</div>
-					<div class="blog-pagination">
-						<?php
-							global $wp_query;
-							$award_big = 999999999; // need an unlikely integer
-							echo paginate_links( array(
-								'base' => str_replace( $award_big, '%#%', esc_url( get_pagenum_link( $award_big ) ) ),
-								'format' => '?paged=%#%',
-								'current' => max( 1, get_query_var('paged') ),
-								'total' => $wp_query->max_num_pages,
-								'prev_next'          => true,
-								'prev_text'          => __('<span class="class="prev_text"><i class="fa fa-caret-left" aria-hidden="true"></i></span>'),
-								'next_text'          => __('<span class="next_text"><i class="fa fa-caret-right" aria-hidden="true"></i></span>'),
 
-							) );
-						?>					
-					</div>
-				
-					<?php else : ?>
-						<p>Post not found</p>
-					<?php endif; ?> 
 				</main><!-- #main -->
 			</div><!-- #primary -->
 		</div>	<!-- /.col-md-9 -->
 	</div><!-- /.row -->
 </div><!-- /.container -->
+
 <?php
 get_footer();
-?>
