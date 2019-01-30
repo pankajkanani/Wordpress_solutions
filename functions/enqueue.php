24d5367016e21998c718ed2713fb56a8f1feab65
pankajcmarix <pankaj.cmarix@gmail.com>
2019-01-30 18:07:18 +0530

    how to enqueue scripts

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -6,6 +6,14 @@
  * @since themename 1.0
  */
 
+
+/*
+**
+** "wp_enqueue_scripts" is used for frontend file
+**
+*/
+add_action('wp_enqueue_scripts', 'themename_frontend_scripts');
+
 function themename_frontend_scripts() {
 	// Load our main stylesheet.
 	wp_enqueue_style( 'themename-style', get_stylesheet_uri(), array(), '1.0.2', 'all' ); // style.css file enquque here.
@@ -20,10 +28,10 @@ function themename_frontend_scripts() {
 
 /*
 **
-** "wp_enqueue_scripts" is used for frontend file
+** "admin_enqueue_scripts" is used for backend file
 **
 */
-add_action('wp_enqueue_scripts', 'themename_frontend_scripts');
+add_action('admin_enqueue_scripts', 'themename_admin_scripts');
 
 function themename_admin_scripts() {
 	// Load our theme stylesheet.
@@ -31,10 +39,4 @@ function themename_admin_scripts() {
 	wp_enqueue_script( 'theme_setting', get_theme_file_uri( '/assets/js/themesetting.js' ), array(), '1.0', true ); // add your custom css/js file 
 	
 }
-/*
-**
-** "admin_enqueue_scripts" is used for backend file
-**
-*/
-add_action('admin_enqueue_scripts', 'themename_admin_scripts');
 ?>
